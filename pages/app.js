cadcb07f9af94bc7a97e28cfb2c5a825ca41efcd
senix.js <farizgavrielalvaro@gmail.com>
2025-07-13 01:03:48 +0700

    Delete pages directory

--- pages/app.js
+++
@@ -1,8 +0,0 @@
-var typed = new Typed(".type", {
-        strings: ["UI/UX Designer.", "Web Developer.", "Front-End Developer."],
-        typeSpeed: 100,
-        backSpeed: 50,
-        loop: true,
-      });
-      
-      
