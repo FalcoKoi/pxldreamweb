319596e8427d412362d2d80653703c3b8c8d4d09
senix.js <farizgavrielalvaro@gmail.com>
2025-07-13 01:04:11 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".type", {
+        strings: ["UI/UX Designer.", "Web Developer.", "Front-End Developer."],
+        typeSpeed: 100,
+        backSpeed: 50,
+        loop: true,
+      });
+      
+      
